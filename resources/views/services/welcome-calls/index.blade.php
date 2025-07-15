dfe57af5a628a86764e9b65f570b59688dd990a1
savenzersofttech <savenzersofttech@gmail.com>
2025-07-15 18:42:19 +0530

    add model now add api fpr data update ans show

--- resources/views/services/welcome-calls/index.blade.php
+++ resources/views/services/welcome-calls/index.blade.php
@@ -30,9 +30,15 @@
 
             <!-- Example DataTable for Dashboard Demo-->
             <div class="card mb-4">
-                <div class="card-header">Calls</div>
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>Calls</span>
+                    <a href="#" class="btn btn-sm btn-primary">
+                        <i class="fa-solid fa-plus"></i> Add New
+                    </a>
+                </div>
+
                 <div class="card-body">
-                    <table id="datatablesSimple">
+                    <table id="datatablesWelcomeCalls">
                         <thead>
                             <tr>
                                 <th>#</th>
@@ -66,7 +72,8 @@
                                 <tr>
                                     <td>{{ $index + 1 }}</td>
                                     <td>
-                                        <a href="#"><strong>{{ Str::ucfirst($call->profile->name) ?? 'N/A' }}</strong></a>
+                                        <a
+                                            href="#"><strong>{{ Str::ucfirst($call->profile->name) ?? 'N/A' }}</strong></a>
 
                                     </td>
                                     <td>
@@ -89,24 +96,20 @@
                                     <td>
                                         <div
                                             class="badge bg-{{ getStatusClass($call->status ?? 'light') }} text-white rounded-pill">
-                                            {{ $call->status ?? '-' }}</div>
-
+                                            {{ $call->status ?? '-' }}
+                                        </div>
                                     </td>
-
                                     <td>
-                                        <button href="{{ route('services.welcome-calls.edit', $call->id) }}"
-                                            class="btn btn-datatable btn-icon btn-transparent-dark me-2">
+
+                                        <button type="button"  data-bs-toggle="modal" data-bs-target="#exampleModalLg"  href="{{ route('services.welcome-calls.edit', $call->id) }}" class="btn btn-datatable btn-icon btn-transparent-dark me-2">
                                             <i class="fa-solid fa-pen-to-square"></i>
                                         </button>
-                                        <button href="{{ route('services.welcome-calls.edit', $call->id) }}"
-                                            class="btn btn-datatable btn-icon btn-transparent-dark me-2">
+                                        <button type="button"  data-bs-toggle="modal" data-bs-target="#exampleModalLg"  href="{{ route('services.welcome-calls.edit', $call->id) }}" class="btn btn-datatable btn-icon btn-transparent-dark me-2">
                                             <i class="fa-solid fa-eye"></i>
                                         </button>
-                                        <button action="{{ route('services.welcome-calls.destroy', $call->id) }}"
-                                            class="btn btn-datatable btn-icon btn-transparent-dark"><i
-                                                class="fa-regular fa-trash-can"></i></button>
-
-
+                                        <button type="button"  data-bs-toggle="modal" data-bs-target="#exampleModalLg"  action="{{ route('services.welcome-calls.destroy', $call->id) }}" class="btn btn-datatable btn-icon btn-transparent-dark">
+                                            <i class="fa-regular fa-trash-can"></i>
+                                        </button>
                                     </td>
                                 </tr>
                             @empty
@@ -120,7 +123,20 @@
             </div>
         </div>
 
-        
+<div class="modal fade" id="exampleModalLg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Large Modal</h5>
+                <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>This is an example of a large modal.</p>
+            </div>
+            <div class="modal-footer"><button class="btn btn-primary" type="button" data-bs-dismiss="modal">Close</button></div>
+        </div>
+    </div>
+</div>
     </main>
 @endsection
 @php
@@ -135,3 +151,65 @@
         };
     }
 @endphp
+
+
+@push('script')
+<script>
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    // https://github.com/fiduswriter/Simple-DataTables/wiki
+
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+    }
+});
+
+    </script>
+<script type="module">
+            import {DataTable} from "../dist/module.js"
+
+            fetch("initial_data.json")
+                .then(response => response.json())
+                .then(data => {
+                    window.dt = new DataTable("#datatablesWelcomeCalls", {
+                        columns: [
+                            {
+                                // select the fourth column ...
+                                select: 3,
+                                // ... let the instance know we have datetimes in it ...
+                                type: "date",
+                                // ... pass the correct datetime format ...
+                                format: "YYYY/DD/MM",
+                                // ... sort it ...
+                                sort: "desc"
+                            }
+                        ],
+                        data: {
+                            headings: [
+                                {
+                                    text: "Name",
+                                    data: "name"
+                                }, {
+                                    text: "Ext.",
+                                    data: "extension"
+                                }, {
+                                    text: "City",
+                                    data: "city"
+                                }, {
+                                    text: "Start date",
+                                    data: "start_date"
+                                }
+                            ],
+                            data
+                        }
+        })
+
+                    fetch("additional_data.json")
+                        .then(response => response.json())
+                        .then(data => window.dt.insert(data))
+                })
+        </script>
+
+
+@endpush
